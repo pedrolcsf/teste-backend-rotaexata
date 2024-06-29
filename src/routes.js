96fb011027a34b3b81cc485b8d4a0206961dc1fe
Pedro Ferreira <pedrolcsferreira@gmail.com>
2024-06-29 09:44:25 -0300

    config migrations db

--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const routes = express.Router();
 
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Hello World' });
+});
+
 module.exports = routes;
