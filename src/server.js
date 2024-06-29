c9cb1861a264dd5211430cdf37fe3ee4b3665938
Pedro Ferreira <pedrolcsferreira@gmail.com>
2024-06-29 09:10:55 -0300

    initial config db, settings

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const routes = require('./routes');
+
+require('./database');
 
 const app = express();
 app.use(express.json());
+app.use(routes);
 
 app.listen(3001, () => console.log('Server is running on port 3001'));
