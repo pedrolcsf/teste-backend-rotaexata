47b99cd7a367db5a9d0f5e5fc72af34ae98d5d3c
Pedro Ferreira <pedrolcsferreira@gmail.com>
2024-06-29 09:24:55 -0300

    add dotenv

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
-
+require('dotenv').config();
 require('./database');
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3001, () => console.log('Server is running on port 3001'));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
