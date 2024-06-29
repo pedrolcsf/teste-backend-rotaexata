6ba8fe6e4042cbade0c17103fee203687630eed2
Pedro Ferreira <pedrolcsferreira@gmail.com>
2024-06-29 08:53:08 -0300

    initial config db, settings

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'rotaexata',
+  password: 'rotaexata',
+  database: 'rotaexata',
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
