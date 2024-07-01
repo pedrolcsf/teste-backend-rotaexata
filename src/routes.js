91e84a18ee4b7467adf9f47fca27c37adc89ad6f
Pedro Ferreira <pedrolcsferreira@gmail.com>
2024-07-01 08:47:54 -0300

    initial tests

--- src/routes.js
+++ src/routes.js
@@ -4,20 +4,20 @@ const UserController = require('./controllers/UserController');
 const AuthController = require('./controllers/AuthController');
 const AddressController = require('./controllers/AddressController');
 
-const protected = require('./middlewares/AuthMiddleware')
+const authMiddleware = require('./middlewares/AuthMiddleware')
 
 const routes = express.Router();
 
 routes.post('/user', UserController.store)
 routes.post('/login', AuthController.store)
 
-routes.post('/addresses', protected, AddressController.store)
-routes.put('/addresses/:id', protected, AddressController.update)
-routes.delete('/addresses/:id', protected, AddressController.delete)
-routes.get('/addresses', protected, AddressController.index)
+routes.post('/addresses', authMiddleware, AddressController.store)
+routes.put('/addresses/:id', authMiddleware, AddressController.update)
+routes.delete('/addresses/:id', authMiddleware, AddressController.delete)
+routes.get('/addresses', authMiddleware, AddressController.index)
 
 // shared
-routes.post('/addresses/:id/share', protected, AddressController.share)
+routes.post('/addresses/:id/share', authMiddleware, AddressController.share)
 routes.get('/shared/:token', AddressController.getSharedAddress)
 
 module.exports = routes;
