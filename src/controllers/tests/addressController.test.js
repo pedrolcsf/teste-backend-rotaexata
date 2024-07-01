d62c3d62c9492b1e6d14652dc9b71a1e2c643896
Pedro Ferreira <pedrolcsferreira@gmail.com>
2024-07-01 11:06:49 -0300

    test update and delete addresses

--- src/controllers/tests/addressController.test.js
+++ src/controllers/tests/addressController.test.js
@@ -36,10 +36,12 @@ afterAll(async () => {
 
 describe('Address Controller', () => {
   let authToken;
+  let userId;
 
   beforeEach(async () => {
     await User.destroy({ where: {} });
     authToken = await getToken()
+    userId = user.id;
   });
 
 
@@ -59,7 +61,64 @@ describe('Address Controller', () => {
 
     expect(response.status).toBe(200);
     expect(response.body).toHaveProperty('id');
-    expect(response.body.zipcode).toBe('12345678');
+    expect(response.body.zipcode).toBe('0000000');
+  });
+
+  it('should update an existing address with valid data', async () => {
+    const address = await Address.create({
+      user_id: userId,
+      zipcode: '0000000',
+      street: 'Test Street',
+      district: 'Test District',
+      city: 'Test City',
+      state: 'ST',
+      complement: 'Apt 1',
+      number: 1223,
+    });
+
+    const response = await request(app)
+      .put(`/addresses/${address.id}`)
+      .set('Authorization', `Bearer ${authToken}`)
+      .send({
+        zipcode: '0000000',
+        street: 'Update Test Street',
+        district: 'Update Test District',
+        city: 'Update Test City',
+        state: 'TS',
+        complement: 'Update Apt 1',
+        number: 1223,
+      });
+
+    expect(response.status).toBe(200);
+    expect(response.body.zipcode).toBe('0000000');
+    expect(response.body.street).toBe('Update Test Street');
+    expect(response.body.district).toBe('Update Test District');
+    expect(response.body.city).toBe('Update Test City');
+    expect(response.body.state).toBe('TS');
+    expect(response.body.complement).toBe('Update Apt 1');
+    expect(response.body.number).toBe(1223);
+  });
+
+  it('should delete an existing address', async () => {
+    const address = await Address.create({
+      user_id: userId,
+      zipcode: '0000000',
+      street: 'Test Street',
+      district: 'Test District',
+      city: 'Test City',
+      state: 'ST',
+      complement: 'Apt 1',
+      number: 1223,
+    });
+
+    const response = await request(app)
+      .delete(`/addresses/${address.id}`)
+      .set('Authorization', `Bearer ${authToken}`);
+
+    expect(response.status).toBe(200);
+
+    const deletedAddress = await Address.findByPk(address.id);
+    expect(deletedAddress).toBeNull();
   });
 });
 
